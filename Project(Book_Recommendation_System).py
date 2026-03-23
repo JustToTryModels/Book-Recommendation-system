afc7fca7fe465a95f5f6d1553410d02e8a6b1dc3
JustToTryModels <zlibbrary7@gmail.com>
2026-03-23 16:56:25 +0530

    Update Project(Book_Recommendation_System).py

--- Project(Book_Recommendation_System).py
+++ Project(Book_Recommendation_System).py
@@ -87,6 +87,22 @@ st.markdown("""
     .stButton button:hover {
         background-color: #45a049;
     }
+    .stFormSubmitButton button {
+        font-family: 'Courier New', Courier, monospace;
+        font-size: 16px;
+        background-color: #4CAF50;
+        color: white !important;
+        border: none;
+        padding: 10px 20px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        margin: 4px 2px;
+        cursor: pointer;
+    }
+    .stFormSubmitButton button:hover {
+        background-color: #45a049;
+    }
     .book-info {
         line-height: 1.2;
         margin-bottom: 15px;
@@ -141,13 +157,15 @@ st.markdown("""
 st.markdown("<p class='subheader'>Let Us Help You Choose Your Next Book!</p>", unsafe_allow_html=True)
 st.image('https://img.freepik.com/premium-vector/bookcase-with-books_182089-197.jpg', use_container_width=True)
 
-# Create a selectbox for book title with autocomplete
+# Create a form to prevent rerun on selectbox change
 all_books = final_filtered_df['title'].unique().tolist()
-book_title = st.selectbox('Enter a book title:', [''] + all_books, key='book_title')
 
-num_recommendations = st.number_input('Enter the number of recommendations:', min_value=1, max_value=50, value=10)
+with st.form(key='recommendation_form'):
+    book_title = st.selectbox('Enter a book title:', [''] + all_books)
+    num_recommendations = st.number_input('Enter the number of recommendations:', min_value=1, max_value=50, value=10)
+    submit_button = st.form_submit_button('Recommend books')
 
-if st.button('Recommend books'):
+if submit_button:
     if book_title:
         if book_title != '':
             similar_books = get_top_similar_books(book_title, num_recommendations)
